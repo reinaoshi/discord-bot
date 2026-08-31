f0f3984a38569c202b6dbdb8b9c0fa47308ee72d
reinaoshi <oshireina@gmail.com>
2026-08-31 04:28:48 +0000

    discord

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send("Pong!")
+
+        bot.run('MTU0MzY3OTM3MDkzMjI1NjkwOQ.Gzf2Xu.AsTVxOMGaXloRcFmPSWAAiUMrr7rbzBlTI-q3k')
